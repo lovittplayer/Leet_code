73d0ec7b3f2f6509125a833f7e5268d198af666a
Lovish Bansal <72393809+lovittplayer@users.noreply.github.com>
2026-06-30 19:14:14 +0530

    Time: 3 ms (61.16%), Space: 12.3 MB (62.91%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+         int n = satisfaction.size();
+        sort(satisfaction.begin(),satisfaction.end());
+         vector <int>like_time_coff(satisfaction.size());
+         for (int i = n-1;i>=0;i--) {
+            int multi = 1;
+            for (int j = i ;j<=n-1;j++)  {
+                like_time_coff[i] += satisfaction[j]*multi;
+                multi++;
+            }
+         }
+
+         int max = INT_MIN;
+         for (int i = 0 ; i<n;i++) {
+            if(like_time_coff[i]>max) max = like_time_coff[i];
+         }
+
+
+        if(max<0) return 0;
+         return max;
+         
+        
+    }
+};
