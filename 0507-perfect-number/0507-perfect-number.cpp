32dfe9dd5b211ccc564c64dd54abb2cc30f46402
Lovish Bansal <72393809+lovittplayer@users.noreply.github.com>
2026-07-14 22:34:10 +0530

    Time: 2 ms (61.07%), Space: 7.6 MB (90.3%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
+        if(num == 1) return false;
         int sum = 1;
         for (int i = 2;i<=sqrt(num);i++) {
                 if(num%i == 0) {
-                    sum += i+ (num/i) ;
+                    sum += i;
+                    if(i!=num/i) {
+                      sum+= (num/i);
+                    }
                 }
         }
-       if(num==sqrt(num)*sqrt(num)) sum -= sqrt(num);
+  
 
         return (sum==num);
     }
