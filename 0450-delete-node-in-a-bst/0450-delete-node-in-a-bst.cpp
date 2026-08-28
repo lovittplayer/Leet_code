c709da171740eb06df6fa2e0f3827e0d58474b52
Lovish Bansal <72393809+lovittplayer@users.noreply.github.com>
2026-08-28 11:38:57 +0530

    Time: 0 ms (100%), Space: 34.3 MB (51.67%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,23 +1,14 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root == NULL) return root;
+        // case 1 : 0 child 
         if(root->val == key) {
             if(root->left == NULL && root->right == NULL) {
                 return NULL;
             }
-
+         // case 2 : 1 child  
             else if (root->left == NULL && root->right!=NULL) {
                 return root->right;
             }
@@ -25,14 +16,15 @@ public:
             else if (root->left != NULL && root->right==NULL) {
                 return root->left;
             }
-
+    // case 3 : when 2 children
             else if (root->left!=NULL && root->right !=NULL) {
                TreeNode * temp  = root->left;
-    
+               // go to inorder pred or suc 
+               // then delete the pred or suc after replacing 
                while(temp->right!=NULL) {
                 temp = temp->right;
                }
-               root->val = temp->val;
+               root->val  = temp->val;
                root->left = deleteNode(root->left,temp->val);
                
             }
